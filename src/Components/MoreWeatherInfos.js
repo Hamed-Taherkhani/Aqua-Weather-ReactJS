054194f37727759bd2539f0a731718d49ccd580a
Hamed Taherkhani <hamedtahi7@gmail.com>
2022-02-24 12:46:14 +0330

    Set sunset and sunrise time.

--- src/Components/MoreWeatherInfos.js
+++ src/Components/MoreWeatherInfos.js
@@ -4,15 +4,38 @@ import "./scss/MoreWeatherInfos.scss";
 function MoreWeatherInfos(props) {
   const { weather } = props;
 
+  let sunrise = null,
+    sunset = null;
+  if (weather.current !== undefined) {
+    sunrise = new Date(weather.current.sunrise * 1000);
+    sunset = new Date(weather.current.sunset * 1000);
+  }
+
   return (
     <div className="more-weather-infos">
       <div className="container">
         <div className="sunrise-sunset flex-row">
-          <section className="sunrise-time time">sunrise 4:51</section>
+          <section className="sunrise-time time">
+            <span>sunrise</span>
+            {sunrise !== null ? (
+              <>
+                {sunrise.getHours().toString().padStart(2, 0)}:
+                {(sunrise.getMinutes() + 1).toString().padStart(2, 0)}
+              </>
+            ) : null}
+          </section>
           <section className="sun-and-orbit">
             <section className="orbit"></section>
           </section>
-          <section className="sunset-time time">sunset 8:00</section>
+          <section className="sunset-time time">
+            <span>sunset</span>
+            {sunset !== null ? (
+              <>
+                {sunset.getHours().toString().padStart(2, 0)}:
+                {(sunset.getMinutes() + 1).toString().padStart(2, 0)}
+              </>
+            ) : null}
+          </section>
         </div>
 
         <table className="further-infos">
