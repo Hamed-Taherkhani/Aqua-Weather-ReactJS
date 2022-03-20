7c94d2125d077e3a5b15fff816b1f3603bf18dce
Hamed Taherkhani <hamedtahi7@gmail.com>
2022-03-20 13:39:15 +0330

    Change background-color in manifest file. Add theme color changer to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -99,21 +99,25 @@ export function determineBackgroundColor(res) {
 
   function changeColor() {
     const now = Math.floor(Date.now() / 1000);
+    const themeColorMeta = document.querySelector("meta[name=theme-color]");
 
     console.log(now, sunrise, sunset);
 
     if (now >= sunrise && now < sunset - 900) {
       background.style.background = "linear-gradient(0, #3ea2e5, #8bc5e3)";
+      themeColorMeta.setAttribute("content", "#8bc5e3");
       background.style.opacity = 1;
       background.style.animation = "opacity 600ms ease 0s 1";
     } else if (now >= sunset - 900 && now <= sunset + 900) {
       background.style.background =
         "linear-gradient(0deg, rgb(254, 192, 81) -83%, rgb(255, 229, 119) 5%, rgb(36, 171, 209) 23%, rgb(39, 56, 94) 106%)";
+      themeColorMeta.setAttribute("content", "rgb(39, 56, 94)");
       background.style.opacity = 1;
       background.style.animation = "opacity 600ms ease 0s 1";
     } else {
       background.style.background =
         "linear-gradient(0deg, rgb(0, 0, 0) -20%, rgb(0 70 126), rgb(39, 56, 94))";
+      themeColorMeta.setAttribute("content", "rgb(39, 56, 94)");
       background.style.opacity = 1;
       background.style.animation = "opacity 600ms ease 0s 1";
     }
